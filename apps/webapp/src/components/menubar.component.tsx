6249918283aabe383e7c2962fb3eaeebd54bd995
fellowseb <wauquier.sebastien@gmail.com>
2025-10-18 19:16:17 +0200

    feat(webapp/components): generic menubar component

---
+++ apps/webapp/src/components/menubar.component.tsx
@@ -0,0 +1,51 @@
+import classNames from 'classnames';
+import { NavLink } from 'react-router';
+import styles from './menubar.module.css';
+import type { IconValue } from './icon.component';
+import Icon from './icon.component';
+
+interface MenuBarItemProps {
+  label: string;
+  icon: IconValue;
+  path: string;
+}
+
+function MenuBarItem(props: MenuBarItemProps) {
+  return (
+    <NavLink
+      to={props.path}
+      relative="path"
+      className={({ isActive }) =>
+        classNames({
+          [styles.tab]: true,
+          [styles.selected]: isActive,
+        })
+      }
+    >
+      <Icon value={props.icon} size="small" mode="primary" />
+      {props.label}
+    </NavLink>
+  );
+}
+
+export interface MenuBarDefinition {
+  items: {
+    label: string;
+    icon: IconValue;
+    path: string;
+  }[];
+}
+
+export interface MenuBarProps {
+  definition: MenuBarDefinition;
+}
+
+export function MenuBar(props: MenuBarProps) {
+  return (
+    <nav className={styles.container}>
+      {props.definition.items.map(({ icon, path, label }) => (
+        <MenuBarItem icon={icon} path={path} label={label} />
+      ))}
+    </nav>
+  );
+}
