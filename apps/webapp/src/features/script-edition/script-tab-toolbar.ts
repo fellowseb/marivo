b8e971ec941d3f002a70d0aff1609ef61ab9e7d3
fellowseb <wauquier.sebastien@gmail.com>
2025-10-18 19:23:28 +0200

    refactor(webapp/script): migrate to new toolbar

---
+++ apps/webapp/src/features/script-edition/script-tab-toolbar.ts
@@ -0,0 +1,22 @@
+import type { ToolbarDefinition } from '../../components/toolbar.component';
+
+export const SCRIPT_TOOLBAR: ToolbarDefinition = {
+  items: [
+    {
+      label: 'undo',
+      icon: 'undo',
+    },
+    {
+      label: 'redo',
+      icon: 'redo',
+    },
+    {
+      label: 'search',
+      icon: 'search',
+    },
+    {
+      label: 'voice command',
+      icon: 'voiceRecording',
+    },
+  ],
+};
