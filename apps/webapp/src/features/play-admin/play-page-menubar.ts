b1d0c7ad26a956ff1e39d520ad85fa6f98d02c2f
fellowseb <wauquier.sebastien@gmail.com>
2025-10-18 19:22:25 +0200

    refactor(webapp/play): migrate to new menubar

---
+++ apps/webapp/src/features/play-admin/play-page-menubar.ts
@@ -0,0 +1,31 @@
+import type { MenuBarDefinition } from '../../components/menubar.component';
+
+export const PLAY_MENU_DEFINITION: MenuBarDefinition = {
+  items: [
+    {
+      label: 'Script',
+      path: '../script',
+      icon: 'script',
+    },
+    {
+      label: 'Staging notes',
+      path: '../staging-directions',
+      icon: 'staging',
+    },
+    {
+      label: 'Blocking',
+      path: '../blocking',
+      icon: 'blocking',
+    },
+    {
+      label: 'Memorization',
+      path: '../memorize',
+      icon: 'memorize',
+    },
+    {
+      label: 'Settings',
+      path: '../settings',
+      icon: 'settings',
+    },
+  ],
+};
