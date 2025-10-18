02cd2bacb0eda187a8aa5d7b8133d3388e2101cd
fellowseb <wauquier.sebastien@gmail.com>
2025-10-18 19:20:13 +0200

    refactor(webapp/blocking): migrate to new toolbar

---
+++ apps/webapp/src/features/blocking/blocking-tab-toolbar.ts
@@ -0,0 +1,22 @@
+import type { ToolbarDefinition } from '../../components/toolbar.component';
+
+export const BLOCKING_TOOLBAR: ToolbarDefinition = {
+  items: [
+    {
+      label: 'undo',
+      icon: 'undo',
+    },
+    {
+      label: 'redo',
+      icon: 'redo',
+    },
+    {
+      label: 'search',
+      icon: 'search',
+    },
+    {
+      label: 'voice command',
+      icon: 'voiceRecording',
+    },
+  ],
+};
