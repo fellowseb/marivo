5ec15d1e4982c8a9c03e9f948b64647654051f66
fellowseb <wauquier.sebastien@gmail.com>
2025-10-18 19:18:01 +0200

    refactor(webapp/memorize): migrate to new toolbar

---
+++ apps/webapp/src/features/lines-memorization/memorize-tab-toolbar.ts
@@ -0,0 +1,10 @@
+import type { ToolbarDefinition } from '../../components/toolbar.component';
+
+export const MEMORIZE_TOOLBAR: ToolbarDefinition = {
+  items: [
+    {
+      label: 'search',
+      icon: 'search',
+    },
+  ],
+};
