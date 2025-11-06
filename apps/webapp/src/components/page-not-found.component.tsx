c8adfafef1d6c35610068dff6b4ec3c9aa388055
fellowseb <wauquier.sebastien@gmail.com>
2025-11-06 02:52:46 +0100

    fix(webapp/page-not-found): fix styling

--- apps/webapp/src/components/page-not-found.component.tsx
+++ apps/webapp/src/components/page-not-found.component.tsx
@@ -1,5 +1,29 @@
+import { NavLink } from 'react-router';
+import styles from './page-not-found.module.css';
+
 function PageNotFound() {
-  return `The page you requested doesn't seem to exist !`;
+  return (
+    <div className={styles.container}>
+      <img
+        src="/src/assets/marivo-app-logo-full-transparent.webp"
+        width={250}
+      />
+      <div className={styles.desc}>
+        The page or resource you requested doesn't seem to exist !<br />
+        You may want to click <a href="https://marivo.app/contact">here</a> to
+        contact us or
+        <br /> click{' '}
+        <NavLink
+          to={{
+            pathname: '/plays',
+          }}
+        >
+          here
+        </NavLink>{' '}
+        to get back to your plays list.
+      </div>
+    </div>
+  );
 }
 
 export default PageNotFound;
