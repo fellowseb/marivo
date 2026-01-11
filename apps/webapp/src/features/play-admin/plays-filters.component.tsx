a947be2b370ea7431638e64a2bc6a5a4a3ec035e
fellowseb <wauquier.sebastien@gmail.com>
2026-01-11 22:13:48 +0100

    refactor(webapp): use CollapsibleSection in PlaysFilters

--- apps/webapp/src/features/play-admin/plays-filters.component.tsx
+++ apps/webapp/src/features/play-admin/plays-filters.component.tsx
@@ -1,9 +1,33 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import styles from './plays-filters.module.css';
 import type { PlayFilterSortOptions, PlaysOrdering } from './plays.lib';
 import Button from '../../components/button.component';
 import Icon from '../../components/icon.component';
 import classNames from 'classnames';
+import CollapsibleSection from '../../components/collapsible-section.component';
+
+interface PlaysFiltersHeaderProps {
+  collapsed: boolean;
+}
+
+function PlaysFiltersHeader(props: PlaysFiltersHeaderProps) {
+  return (
+    <div className={styles.header}>
+      <span className={styles.headerTitle}>
+        <Icon value="filter" size="small" mode="primary" />
+        Filters
+      </span>
+      <span
+        className={classNames({
+          [styles.arrow]: true,
+          [styles.arrowDown]: !props.collapsed,
+        })}
+      >
+        ▶
+      </span>
+    </div>
+  );
+}
 
 export interface PlaysFiltersProps {
   filters: PlayFilterSortOptions;
@@ -40,95 +64,77 @@ function PlaysFilters(props: PlaysFiltersProps) {
     }
     handleFiltersChange();
   };
-  const [showFilters, setShowFilters] = useState(false);
-  const handleShowFilters = () => {
-    setShowFilters((prev) => !prev);
-  };
 
   return (
     <div className={styles.playFilters}>
-      <div className={styles.header}>
-        <Button onClick={handleShowFilters} variant="discrete">
-          <span className={styles.headerTitle}>
-            <Icon value="filter" size="small" mode="primary" />
-            Filters
-          </span>
-          <span
-            className={classNames({
-              [styles.arrow]: true,
-              [styles.arrowDown]: showFilters,
-            })}
-          >
-            ▶
-          </span>
-        </Button>
-      </div>
-      <div
-        className={classNames({
-          [styles.content]: true,
-          [styles.hidden]: !showFilters,
-        })}
+      <CollapsibleSection
+        Header={<PlaysFiltersHeader collapsed={true} />}
+        HeaderExpanded={<PlaysFiltersHeader collapsed={false} />}
       >
-        <div className={styles.formGrid}>
-          <label>Title:</label>
-          <input
-            type="text"
-            id="filter-title"
-            onChange={handleFiltersChange}
-            ref={titleFilterRef}
-            size={16}
-            value={props.filters.title}
-          />
-          <label>Sort by:</label>
-          <select
-            onChange={handleFiltersChange}
-            ref={orderByRef}
-            value={props.filters.orderBy}
-          >
-            <button>
-              <selectedcontent></selectedcontent>
-            </button>
-            <option
-              value={'orderByLastModificationDateAsc' satisfies PlaysOrdering}
+        <div className={styles.content}>
+          <div className={styles.formGrid}>
+            <label>Title:</label>
+            <input
+              type="text"
+              id="filter-title"
+              onChange={handleFiltersChange}
+              ref={titleFilterRef}
+              size={16}
+              value={props.filters.title}
+            />
+            <label>Sort by:</label>
+            <select
+              onChange={handleFiltersChange}
+              ref={orderByRef}
+              value={props.filters.orderBy}
             >
-              Last modification date
-              <Icon value="asc" size="small" mode="primary" />
-            </option>
-            <option
-              value={'orderByLastModificationDateDesc' satisfies PlaysOrdering}
-            >
-              Last modification date
-              <Icon value="desc" size="small" mode="primary" />
-            </option>
-            <option value={'orderByCreationDateAsc' satisfies PlaysOrdering}>
-              Creation date
-              <Icon value="asc" size="small" mode="primary" />
-            </option>
-            <option value={'orderByCreationDateDesc' satisfies PlaysOrdering}>
-              Creation date
-              <Icon value="desc" size="small" mode="primary" />
-            </option>
-            <option value={'orderByTitleAsc' satisfies PlaysOrdering}>
-              Title
-              <Icon value="asc" size="small" mode="primary" />
-            </option>
-            <option value={'orderByTitleDesc' satisfies PlaysOrdering}>
-              Title
-              <Icon value="desc" size="small" mode="primary" />
-            </option>
-          </select>
-          <label>Plays I own only</label>
-          <input
-            type="checkbox"
-            onChange={handleFiltersChange}
-            ref={onlyPlaysSelfOwnsRef}
-            checked={props.filters.onlyPlaysSelfOwns}
-          />
+              <button>
+                <selectedcontent></selectedcontent>
+              </button>
+              <option
+                value={'orderByLastModificationDateAsc' satisfies PlaysOrdering}
+              >
+                Last modification date
+                <Icon value="asc" size="small" mode="primary" />
+              </option>
+              <option
+                value={
+                  'orderByLastModificationDateDesc' satisfies PlaysOrdering
+                }
+              >
+                Last modification date
+                <Icon value="desc" size="small" mode="primary" />
+              </option>
+              <option value={'orderByCreationDateAsc' satisfies PlaysOrdering}>
+                Creation date
+                <Icon value="asc" size="small" mode="primary" />
+              </option>
+              <option value={'orderByCreationDateDesc' satisfies PlaysOrdering}>
+                Creation date
+                <Icon value="desc" size="small" mode="primary" />
+              </option>
+              <option value={'orderByTitleAsc' satisfies PlaysOrdering}>
+                Title
+                <Icon value="asc" size="small" mode="primary" />
+              </option>
+              <option value={'orderByTitleDesc' satisfies PlaysOrdering}>
+                Title
+                <Icon value="desc" size="small" mode="primary" />
+              </option>
+            </select>
+            <label>Plays I own only</label>
+            <input
+              type="checkbox"
+              onChange={handleFiltersChange}
+              ref={onlyPlaysSelfOwnsRef}
+              checked={props.filters.onlyPlaysSelfOwns}
+            />
+          </div>
+          <Button onClick={handleClearClick} icon="clear">
+            Clear
+          </Button>
         </div>
-        <Button onClick={handleClearClick} icon="clear">
-          Clear
-        </Button>
-      </div>
+      </CollapsibleSection>
     </div>
   );
 }
